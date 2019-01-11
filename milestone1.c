9f198d5bbd1730972e5d929201e83faf0aa764a6
Sigurd Holmen <sigurd.holm1@gmail.com>
2019-01-11 09:39:33 +0100

    Added first milestone

---
+++ milestone1.c
@@ -0,0 +1,35 @@
+
+
+
+int main()
+{
+	// Declarations
+
+
+	// Set up socket
+
+
+	// Bind to port
+
+
+	// Demonize
+
+
+	// Main loop
+	while(1){
+		// Listen
+
+		// Child
+		if(fork()==0){
+			// Read responce
+
+			// Respond
+		}
+		// Parent
+		else{
+
+		}
+	}
+
+	return 0;
+}
