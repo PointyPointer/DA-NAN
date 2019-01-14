b3f83f9c83bd84ec12074c02682e901161b566ca
Sigurd Holmen <sigurd.holm1@gmail.com>
2019-01-14 09:16:30 +0100

    Broken, fix i/o

--- milestone1/milestone1.c
+++ milestone1/milestone1.c
@@ -8,24 +8,36 @@
 #define QUEUE_SIZE 20
 #define PORT 80
 #define GET_HEAD_SIZE 2048
+#define UID 1000
+#define GID 1000
 
 int socketSetup();
 
 int daemonize();
 
-int writeHeader(int, int);
+// int writeHeader(int, int);
+
+int writeBody(int, char*);
+
+// char* getContentType();
 
 
 int main(){
 	// Declarations
 	int sd, client_sd;
 	char buf[GET_HEAD_SIZE];
+	FILE *err;
+
+	char *httpMethod;
+	char *filePath;
+
+	err = fopen("error.log", "a");
 
 	// initialation of socket and binding
 	sd = socketSetup();
 
 	// Turn the process to a daemon
-	daemonize();
+	//daemonize();
 
 	listen(sd, QUEUE_SIZE);
 	// Main loop
@@ -34,13 +46,15 @@ int main(){
 
 		// Child
 		if(fork()==0){
+			httpMethod = malloc(7);
 			// Read request
-			read(client_sd, buf, GET_HEAD_SIZE);
+			//parseRequest(client_sd, )
+			// read(, buf, GET_HEAD_SIZE);
 
 			
 			// Respond
-			writeHeader(client_sd, 200)
-
+			//writeHeader(client_sd, 200)
+			writeBody(client_sd, "test.asis");
 
 			// terminate child
 			shutdown(client_sd, SHUT_RDWR);
@@ -70,19 +84,61 @@ int socketSetup(){
 		exit(1);		
 	}
 
+	setuid( (uid_t) UID );
+	setgid( (gid_t) GID );
 
 	return sd;
 }
 
 int deamonize(){
 
+	if(fork()){
+		exit(0); //parent dies
+	}
+	setsid(); // Create session, free from tty
+	signal(SIGTTOU, SIG_IGN); // 
+	signal(SIGTTIN, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN); 
+
+	if(fork()){
+		exit(0); //sessionleader dies, cannot obtain tty
+	}
+
+	close(0);
+	close(1);
 
 	return 0;
 }
 
-int write_header(int sd, int status){
-	dprintf(sd, "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n");
+// int write_header(int sd, int status){
+// 	write(sd, "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n", 42);
 
-	return 0;
+// 	return 0;
+// }
+
+int write_body(int sd, char* filePath){
+	FILE *fp;
+	int buffSize = 1024;
+	char* buff[buffSize];
+	int l;
+
+	fp = fopen(filePath, "r");
+
+	if(fp==0){
+		dprintf(sd, "HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\nFile not found");
+	}
+	else{
+		l = 1;
+		while(l){
+			l = fread(buff, 1, buffSize, sd);
+			fwrite(buff, 1, l, sd);			
+		}
+
+	}
+
 }
 
+// char* getContentType(){
+
+// 	return 
+// }
