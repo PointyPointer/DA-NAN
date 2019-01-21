f3bd1736b607c7d8c0ed1b3a74a50f5497ef48db
Magnus Haukeboe <mhaukeboe@hotmail.com>
2019-01-21 13:16:31 +0100

    new log and stuff

--- milestone2/dock.sh
+++ milestone2/dock.sh
@@ -1,8 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 gcc --static -o etc/web milestone2.c
 touch error.log
-docker build -t web .
+docker build -f server -t web .
 rm -f error.log
 
 docker run -d --cap-drop ALL --cap-add NET_BIND_SERVICE --cap-add SETGID --cap-add SETUID --cap-add SYS_CHROOT -v log_volum:/var/log --cpu-shares=50 --name "web" -p 80:80 web
