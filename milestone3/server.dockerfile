724477e87c51b1fdfca1909f066e5ad809e31dd5
Magnus Haukeboe <mhaukeboe@hotmail.com>
2019-01-25 13:34:28 +0100

    fixies

--- milestone3/server.dockerfile
+++ milestone3/server.dockerfile
@@ -2,8 +2,8 @@
 
 FROM scratch
 
-COPY etc/* /etc/
-COPY www/* /var/www/
+COPY milestone3/etc/* /etc/
+COPY milestone3/www/* /var/www/
 
 COPY error.log /var/log/web_error.log
 COPY access.log /var/log/web_access.log
--- mp3.sh
+++ mp3.sh
@@ -2,11 +2,11 @@
 
 gcc --static -o milestone3/etc/web milestone3/milestone3.c
 
-touch error.log milestone3/access.log
-docker build -f milestone3/server.dockerfile -t mp3_web .
-docker build -f milestone3/alpine.dockerfile -t mp3_alpine .
-rm -f error.log milestone3/access.log
+touch error.log access.log
+docker build -f milestone3/server.dockerfile -t ms3_web .
+#docker build -f milestone3/alpine.dockerfile -t ms3_alpine .
+rm -f error.log access.log
 
-docker run -d --cap-drop ALL --cap-add NET_BIND_SERVICE --cap-add SETGID --cap-add SETUID --cap-add SYS_CHROOT --mount type=volume,src=log,dst=/var/log/ --cpu-shares=50 --name "web" -p 80:80 mp3_web
+docker run -d --cap-drop ALL --cap-add NET_BIND_SERVICE --cap-add SETGID --cap-add SETUID --cap-add SYS_CHROOT --mount type=volume,src=log,dst=/var/log/ --cpu-shares=50 --name "web" -p 80:80 ms3_web
 
-docker run -p 8080:80 mp3_alpine
+#docker run -p 8080:80 mp3_alpine
