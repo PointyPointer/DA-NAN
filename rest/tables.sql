aa20fcdf641d60925f66e4649416b564756f1e39
Sigurd Holmen <sigurd.holm1@gmail.com>
2019-01-27 23:29:46 +0100

    Added sql file to create DB

---
+++ rest/tables.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Forfatter (
+
+  forfatterID  SMALLINT(5),
+  fornavn      VARCHAR(100),
+  etternavn    VARCHAR(100),
+  nasjonalitet VARCHAR(100),
+
+  PRIMARY KEY (forfatterID)
+);
+
+CREATE TABLE Bok (
+
+  bokID       SMALLINT(5),
+  tittel      VARCHAR(250),
+  forfatterID SMALLINT(5),
+
+  PRIMARY KEY (bokID),
+  FOREIGN KEY (forfatterID) REFERENCES Forfatter (forfatterID)
+);
+
+CREATE TABLE Bruker ( 
+  brukerID SMALLINT(5), 
+  passordhash TEXT(64), 
+  fornavn VARCHAR(100), 
+  etternavn VARCHAR(100),
+
+  PRIMARY KEY (forfatterID)
+);
+
+CREATE TABLE Sesjon ( 
+  sesjonsID TEXT(128), 
+  brukerID SMALLINT(5),
+
+  PRIMARY KEY (sesjonsID),
+  FOREIGN KEY (brukerID) REFERENCES Bruker (brukerID)
+);
