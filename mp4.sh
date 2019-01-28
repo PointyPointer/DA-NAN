cdaf2bfec33d96d30e18d5829b2b8bf24b1de160
Sigurd Holmen <sigurd.holm1@gmail.com>
2019-01-28 09:12:04 +0100

    Add app.js

--- mp4.sh
+++ mp4.sh
@@ -8,4 +8,4 @@ docker build -f cgi/alpine.dockerfile -t alpine .
 docker build -f rest/rest.dockerfile -t rest .
 rm -f error.log web/access.log
 
-docker run -d --cap-drop ALL --cap-add NET_BIND_SERVICE --cap-add SETGID --cap-add SETUID --cap-add SYS_CHROOT --mount type=volume,src=log,dst=/var/log/ --cpu-shares=50 --name "web" -p 80:80 web
+docker run  --mount type=volume,src=log,dst=/var/log/ --cpu-shares=15 --name "rest" -p "1337:1337" rest
--- rest/rest.dockerfile
+++ rest/rest.dockerfile
@@ -2,6 +2,18 @@
 
 FROM node:alpine
 
-RUN ["apk", "run", "--update", "sqlite"]
+RUN ["apk", "update"]
+RUN ["apk", "upgrade"]
+
+RUN ["apk", "add", "--update", "sqlite"]
+
+WORKDIR /app
+COPY rest/*.json /app/
+
+RUN ["npm", "install"]
+
+COPY rest/app.js /app/app.js
+EXPOSE 1337
+CMD node app.js
 
 
