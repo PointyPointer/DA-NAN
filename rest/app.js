450269e31268a9c1e741299506bd7f6fadfbddd8
Magnus Haukeboe <mhaukeboe@hotmail.com>
2019-02-01 23:38:00 +0100

    Moar fixies

--- rest/app.js
+++ rest/app.js
@@ -1,3 +1,5 @@
+const express = require('express')
+const app = express()
 const o2x = require('object-to-xml')
 const parseString = require('xml2js').parseString
 const bodyParser = require("body-parser")
@@ -11,7 +13,12 @@ app.use(xmlparser());
 
 const sqlite3 = require('sqlite3').verbose()
 // const h = new XMLHttpRequest()Responsen
-
+app.use((req,res,next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+	res.header('Access-Control-Allow-Headers', 'content-type')
+	next()
+})
 app.get('/', (req, res) => res.send('Hello World!'))	
 
 app.get('/forfatter', (req, res) => {
@@ -250,14 +257,14 @@ app.delete('/:tablename', (req, res) => {
 
 app.delete('/:tablename/:id', (req, res) => {
 	let db = new sqlite3.Database('/db/potatoDB.db')
-	
+	let idname	
 	let name = req.params['tablename'].toLowerCase()
 	let id = req.params['id']
 	if(name === 'forfatter' || name === 'bok'){
 		if(name === 'forfatter')
-			let idName = 'forfatterID'
+			idName = 'forfatterID'
 		else
-			let idName = 'bokID'
+			idName = 'bokID'
 
 
 		db.serialize(() => {
@@ -299,9 +306,10 @@ app.post('/signup', (req, res) => {
        })
        stmt.finalize()
 
+		})
+		db.close()
 	})
-	db.close()
-}
+})
 
 app.post('/signin', (req, res) => {
 	let db = new sqlite3.Database('/db/potatoDB.db')
@@ -310,25 +318,25 @@ app.post('/signin', (req, res) => {
 	let hashpwd
 
 	db.serialize(() => {
-		let stmt = db.prepare('SELECT fornavn, passordhash FROM Bruker WHERE fornavn = ((?))'), err => {
+		let stmt = db.prepare('SELECT fornavn, passordhash FROM Bruker WHERE fornavn = ((?))', err => {
 			if(err) console.log('DB prepare', err)
 		})
 		stmt.get(username, [], (err, row) => {
 			if(err) console.log(err)
 			hashpwd = row.passordhash			
 		})	
-	}
-	bcrypt.compare(clearpwd, hashpwd, (err, res) {
+	})
+	bcrypt.compare(clearpwd, hashpwd, (err, res) => {
 		if(res === true) {
 			db.serialize(() => {
-				let stmt = db.prepare	
+			})	
 		}	
 	})
 	db.close()
-}
+})
 
 app.get('/logout', (req, res) => {
 
-}
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
