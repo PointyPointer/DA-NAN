59fb936772f2c67f1f2394f6653488c359b2cb63
Snapdraco <pgustad@me.com>
2019-02-08 12:10:00 +0100

    Login test greier, heng me d funke ikke

---
+++ cgi/cgi/logintest.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+import cgi
+import os
+import cgitb
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+bnavn = form.getvalue('brukernavn')
+pord = form.getvalue('passord')
+
+print ('Content-Type: text/html; charset=utf-8\n')
+print ()
+print ('<H1>Test for faen.</H1>')
+print ('Heng meg')
+
