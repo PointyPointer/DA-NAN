184c08ed2e7f1fbea61430cf085ce2f019e29918
Magnus Haukeboe <mhaukeboe@hotmail.com>
2019-03-14 08:47:21 +0100

    Android cookie

--- android/app/src/main/java/com/testmaskin/android/MainActivity.java
+++ android/app/src/main/java/com/testmaskin/android/MainActivity.java
@@ -3,13 +3,16 @@ package com.testmaskin.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public class MyWebViewClient extends WebViewClient {}
 
     WebView wv;
+    String cookies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        wv = findViewById(R.id.web_view);
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.loadUrl("http://testmaskin:80/");
-        wv.setWebViewClient(new WebViewClient());
-
         String firstName = "Firstname";
         String lastName = "Lastname";
 
+        wv = findViewById(R.id.web_view);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("http://testmaskin:80/");
+        wv.setWebViewClient(new MyWebViewClient() {
+            @Override
+            public void onPageFinished (WebView view, String url) {
+                cookies = CookieManager.getInstance().getCookie(url);
+            }
+        });
+
         TextView name = findViewById(R.id.toolbar_title);
-        name.setText(firstName + " " + lastName);
+//        name.setText(firstName + " " + lastName);
+        name.setText(cookies);
     }
 
     @Override
