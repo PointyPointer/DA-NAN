541cd28bf7b6b43d5360441dd24b749ec314e96b
Sigurd Holmen <sigurd.holm1@gmail.com>
2019-03-17 23:49:56 +0100

    Fixing put/post in api

--- rest/app.js
+++ rest/app.js
@@ -35,13 +35,15 @@ app.use((req,res,next) => {
 // Needed for type checking when inserting data in sqlite3 DB
 // which have no strict typematching
 function checkValue(val, type='string'){
-	if(val && typeof val === 'object' && Array.isArray(val))
-		return val.every(entry => typeof entry === type || typeof entry === 'number' && Number(entry))
-	
-	else if(val && typeof val === type)
-		return true
+	return true
 
-	return false
+ //  if(val && typeof val === 'object' && Array.isArray(val))
+	// 	return val.every(entry => typeof entry === type || typeof entry === 'number' && Number(entry))
+	
+	// else if(val && typeof val === type)
+	// 	return true
+
+	// return false
 }
 
 function logoutUser(res, status=401) {
